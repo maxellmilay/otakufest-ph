0a3bf79d4a3482bc811cd75744b38c7b3f3c8b07
maxellmilay <milaymaxell@gmail.com>
2023-10-13 01:27:18 +0800

    [Feature][Maxell] added navbar and home page components

---
+++ components/TheHeader.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'next/link';
+import Routes from '@/enums/routes';
+
+const TheHeader = () => {
+  return (
+    <header className="bg-site-blue-100/70 fixed flex w-screen px-10 py-5 justify-between">
+      <h1 className="text-3xl">OTAKUFEST 2023</h1>
+      <nav className="flex items-center gap-3">
+        <Link href={Routes.GUESTS}>Guests</Link>
+        <Link href={Routes.SHIRT}>Shirt</Link>
+        <Link href={Routes.EVENTS}>Events</Link>
+        <Link href={Routes.FAQS}>FAQS</Link>
+        <Link href={Routes.CONTACT_US}>Contact Us</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default TheHeader;
---
+++ enums/routes.ts
@@ -0,0 +1,10 @@
+enum Routes {
+  HOME = '/',
+  GUESTS = '/guests',
+  SHIRT = 'shirt',
+  FAQS = '/faqs',
+  EVENTS = '/events',
+  CONTACT_US = '/contact-us',
+}
+
+export default Routes;
