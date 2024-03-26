76fe71623d84fcab99ceafe3a7546e533a3d20e0
Loweso <dlouisefermin@gmail.com>
2024-03-27 01:04:13 +0800

    [UI] Added event description

---
+++ components/EventDescription.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const EventDescription = () => {
+  return (
+    <div className="flex flex-col items-center justify-around text-center bg-[#C60001] h-96 w-full p-20">
+      <span className="text-6xl">OTAKUFEST</span>
+      <span className="text-xl">
+        Knock, knock. Who’s there? Otakufest. Otakufest who? Otakufest 2024!
+        Charot. Unsa bitaw description ari? Happy birthday, Maxell Gahit Milay.
+        Knock, knock. Who’s there? Otakufest. Otakufest who? Otakufest 2024!
+        Charot. Unsa bitaw description ari? Happy birthday, Maxell Gahit Milay.
+      </span>
+    </div>
+  );
+};
+
+export default EventDescription;
