63bc19f8b9fe6de460e10ef03f5a5125f7ace54f
Maxell Milay <milaymaxell@gmail.com>
2024-06-16 12:27:17 +0800

    [Feature][Maxell] added cdn links to images

---
+++ enums/imageUrls.ts
@@ -0,0 +1,12 @@
+export const OF24_BACKGROUND =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718507762/2024-custombg_lxqwxq.png';
+export const OF24_PAN =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718508039/of24_pan_uhi8tv.png';
+export const OF24_SHRIMP =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718508268/shrimp_twwexp.png';
+export const OF24_LEAF =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718508348/leaf_zjqbje.png';
+export const OF24_BBQ =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718508468/bbq_vdtbmb.png';
+export const OF24_FISH =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718508519/fish_ypoxw6.png';
