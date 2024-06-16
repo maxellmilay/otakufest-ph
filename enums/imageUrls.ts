1434ff78e3d54857af6fab6deb797b216ca68b33
Maxell Milay <milaymaxell@gmail.com>
2024-06-16 14:09:31 +0800

    [Adjustment][Maxell] refactored enums

--- enums/imageUrls.ts
+++ enums/imageUrls.ts
@@ -10,3 +10,119 @@ export const OF24_BBQ =
     'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718508468/bbq_vdtbmb.png';
 export const OF24_FISH =
     'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718508519/fish_ypoxw6.png';
+export const OF24_CAROUSEL_1 =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718514049/hero-bg_rlznav.png';
+export const OF24_CAROUSEL_2 =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718513982/sponsors-bg_gg7bod.png';
+export const LOGO =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517639/logo_rsvqpz.png';
+export const OF24_LOGO =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718514119/logo-2024_y5o356.png';
+export const OF24_LOGO_WHITE =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718514125/logo-2024-white_igbjom.png';
+export const OF24_MAIN_VISUAL =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718514563/of24visual_bdbs49.png';
+export const OF24_TICKETS =
+    'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517089/tickets_fokjkg.jpg';
+
+export const EVENTS = {
+    SOLO_SKIT: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515389/solo_coskit_m0l3yt.png',
+        eventName: 'Solo Skit Cosplay Competition',
+    },
+    DUO_SKIT: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515391/duo_coskit_qtbaxi.png',
+        eventName: 'Duo Skit Cosplay Competition',
+    },
+    SOLO_DUO_ASIAN_GROOVE: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515392/ags_solo_duo_xbzyna.png',
+        eventName: 'Solo/Duo Asian Groove Showdown',
+    },
+    GROUP_ASIAN_GROOVE: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515392/ags_group_qsovxw.png',
+        eventName: 'Group Asian Groove Showdown',
+    },
+    KARAOKE_JAPANESE_SINGING: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515390/japsing_nwqgsb.png',
+        eventName: 'Kara-OK!! Japanese Singing Competition',
+    },
+    ORIGINAL_CHARACTER_DESIGN: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515390/ocd_llmgxw.png',
+        eventName: 'Original Character Design Competition',
+    },
+    ONE_SHOT_COMIC: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515390/osc_xdnzab.png',
+        eventName: 'One-Shot Comic Competition',
+    },
+    ANIMATION: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515391/animotion_gif_rg0uj7.gif',
+        eventName: 'AniMotion! Animation Competition',
+    },
+    COMING_SOON: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515391/coming_soon_por0sv.png',
+        eventName: '??? Competition',
+    },
+};
+
+export const PARTNERED_EVENTS = {
+    SABERSTURM: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515390/sabersturm_szozxk.png',
+        eventName: 'Sabersturm Competition',
+    },
+    YUGIOH: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515388/ygo_blk_xxxs80.png',
+        eventName: '??? Competition',
+    },
+    VANGUARD: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515391/cfv_blk_wimd03.png',
+        eventName: '??? Competition',
+    },
+    GUNPLA: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515390/gunpla_blk_zqww85.png',
+        eventName: '??? Competition',
+    },
+    DND: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515390/dnd_blk_b8zvvt.png',
+        eventName: '??? Competition',
+    },
+    COMING_SOON: {
+        eventUrl:
+            'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718515391/coming_soon_por0sv.png',
+        eventName: '??? Competition',
+    },
+};
+
+export enum SPONSOR {
+    SELECTA = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517219/selecta_czn9q1.png',
+    MY_LANGUAGE_CAFE = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517248/my-language-cafe_gtzvmf.png',
+    PNB = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517220/pnb_fwe2ur.png',
+    INKPLACE = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517248/inkplace_bfjrcp.png',
+    HOBBIWORKZ = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517249/hobbiworkz_dw6lsq.png',
+    MACOLOGY = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517248/macology_ednfrw.png',
+    NATURES_SPRING = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517248/natures-spring_pdnrd4.png',
+    WORKPLACE_CAFE = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517219/workplace-cafe_a75udk.png',
+    ANE_MI = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517253/ane-mi_gvknyb.png',
+    SM_SEASIDE = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517219/sm-seaside_hx4r1p.png',
+    PROFOOD = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517220/profood_gjbufp.png',
+    PHOENIX = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517229/phoenix_fxyt4e.png',
+    NIKKE = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517248/nikke_bdnsot.png',
+    NONKI = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517245/nonki_jtdf3z.png',
+    ANGKAS = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517249/angkas_hxmkoj.png',
+    SUNSTAR = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517219/sunstar_sknz7d.png',
+    KEEPSAKES = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517248/keepsakes_v042r5.png',
+    RADIO_105 = 'https://res.cloudinary.com/dhyoibvtc/image/upload/v1718517220/radio-105_tjfvnt.png',
+}
