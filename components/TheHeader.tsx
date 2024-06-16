37a2f2ceb6f50f705ce4225b7be7cc84e5dbf979
Maxell Milay <milaymaxell@gmail.com>
2024-06-16 12:50:12 +0800

    [Adjustment][Maxell] added animation in header

--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -7,12 +7,12 @@ import { FaBars } from 'react-icons/fa';
 
 const TheHeader = () => {
     return (
-        <header className="bg-site-main fixed z-20 flex items-center h-16 w-screen px-10 justify-between">
+        <header className="bg-site-main fixed z-20 flex items-center py-4 w-screen px-10 justify-between">
             <Link
                 href={Routes.HOME}
                 className="flex items-center text-2xl font-bold md:text-4xl"
             >
-                <figure className="relative aspect-square w-8 md:w-14">
+                <figure className="relative aspect-square w-8 md:w-14 hover:-translate-y-1 duration-200">
                     <Image
                         src={enums.ImageUrl.LOGO2024_WHITE}
                         alt="guest"
@@ -20,15 +20,35 @@ const TheHeader = () => {
                         className="object-cover"
                     />
                 </figure>
-                OTAKUFEST
+                <p className="hover:-translate-y-1 duration-200">OTAKUFEST</p>
             </Link>
             <nav className="hidden md:flex items-center gap-5 text-lg">
-                <Link href="/#guestSection">Guests</Link>
-                <Link href="/#shirtSection">Shirt</Link>
-                <Link href="/#eventSection">Events</Link>
-                <Link href={Routes.FAQS}>FAQs</Link>
+                <Link
+                    href="/#guestSection"
+                    className="hover:-translate-y-1 duration-200"
+                >
+                    Guests
+                </Link>
+                <Link
+                    href="/#shirtSection"
+                    className="hover:-translate-y-1 duration-200"
+                >
+                    Merch
+                </Link>
+                <Link
+                    href="/#eventSection"
+                    className="hover:-translate-y-1 duration-200"
+                >
+                    Events
+                </Link>
+                <Link
+                    href={Routes.FAQS}
+                    className="hover:-translate-y-1 duration-200"
+                >
+                    FAQs
+                </Link>
                 <Link href={Routes.CONTACT_US} className="border-lg">
-                    <div className="bg-white px-5 py-2 rounded-lg text-site-main border-2 hover:bg-site-main hover:border-site-secondary hover:text-site-secondary">
+                    <div className="bg-white px-5 py-2 rounded-lg text-site-main border hover:bg-site-main hover:border-site-secondary hover:text-site-secondary duration-200">
                         Contact Us
                     </div>
                 </Link>
