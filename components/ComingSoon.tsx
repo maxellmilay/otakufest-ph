706fb45daa3f053f55988b92b0cdc4abc28d5a61
Maxell Milay <milaymaxell@gmail.com>
2024-06-17 07:43:14 +0800

    [Adjustment][Maxell] fix responsiveness of coming soon component

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const VtuberSection = () => {
     return (
-        <div className="flex flex-col items-center justify-center text-center h-[30vh] md:h-[75vh] w-screen my-20 relative bg-cover bg-center text-site-main bg-theme">
-            <span className="text-2xl md:text-4xl font-bold px-10 w-full">
+        <div className="flex flex-col items-center justify-center text-center h-[30vh] md:h-[75vh] w-screen my-10 md:my-16 lg:my-20 relative bg-cover bg-center text-site-main bg-theme">
+            <span className="text-2xl md:text-4xl font-bold px-10 w-ful">
                 COMING SOON!
             </span>
             <span className="md:text-[1.5vw] z-10">
