e126c3931c555d4d2633a72617b1e2be8d3d1b27
Maxell Milay <milaymaxell@gmail.com>
2024-06-17 07:43:44 +0800

    [Adjustment][Maxell] fixed responsiveness of guest setcion

--- components/GuestSection.tsx
+++ components/GuestSection.tsx
@@ -3,11 +3,9 @@ import ComingSoon from './ComingSoon';
 
 const GuestSection = () => {
     return (
-        <div className="flex flex-col items-center justify-around text-justify bg-white w-full p-10 md:p-20">
+        <div className="flex flex-col items-center justify-around text-justify bg-white w-full p-10 md:px-10 md:py-16 lg:p-20">
             <div className="flex flex-col text-site-main">
-                <span className="text-4xl md:text-6xl font-bold w-full">
-                    GUESTS
-                </span>
+                <p className="text-4xl lg:text-6xl font-bold w-full">GUESTS</p>
             </div>
             <ComingSoon />
         </div>
