fe9ef13ac353e6a926a74bb7a0563bff8f8ca0bb
Maxell Milay <milaymaxell@gmail.com>
2024-06-16 11:04:07 +0800

    [Refactor][Maxell] remove unsued route enums

--- enums/routes.ts
+++ enums/routes.ts
@@ -1,12 +1,7 @@
 enum Routes {
     HOME = '/',
-    GUESTS = '/guests',
-    SHIRT = '/shirt',
     FAQS = '/faqs',
-    EVENTS = '/events',
     CONTACT_US = '/contact-us',
-    UPDATES = '/updates',
-    SUPPORT = '/support',
 }
 
 export default Routes;
