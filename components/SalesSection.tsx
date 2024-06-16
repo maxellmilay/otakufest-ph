c6272b27e2b45e000c966514fe08aaaca29bd008
Maxell Milay <milaymaxell@gmail.com>
2024-06-16 21:39:14 +0800

    [Adjustment][Maxell] fix sales section responsiveness

--- components/SalesSection.tsx
+++ components/SalesSection.tsx
@@ -8,9 +8,9 @@ const SalesSection = () => {
     return (
         <div className="flex flex-col items-center justify-evenly bg-white w-full p-10 gap-10 md:p-20 md:px-[10vw]">
             <div className="flex flex-col text-site-main">
-                <span className="text-5xl md:text-8xl font-bold w-full">
+                <p className="text-4xl md:text-6xl font-bold w-full text-center">
                     TICKETS & MERCH
-                </span>
+                </p>
             </div>
             <div className="flex flex-col md:flex-row w-full bg-site-main rounded-xl overflow-hidden hover:scale-[1.01] transform transition duration-y">
                 <figure className="relative w-auto h-64 md:w-1/2 md:h-auto">
@@ -23,20 +23,20 @@ const SalesSection = () => {
                 </figure>
                 <div className="flex flex-col justify-between w-full gap-10 p-8 md:w-2/3">
                     <div className="flex flex-col">
-                        <span className="text-2xl md:text-5xl font-bold w-full">
+                        <p className="text-xl md:text-3xl font-bold w-full">
                             TICKETS
-                        </span>
-                        <span className="text-md md:text-xl mt-4">
+                        </p>
+                        <p className="text-xs sm:text-sm md:text-md mt-4">
                             Secure your tickets now for an unforgettable
                             experience packed with incredible panels, exclusive
                             screenings, and epic cosplay. Whether you&apos;re a
                             long-time fan or new to the scene, Otakufest 2024
                             promises excitement.
-                        </span>
+                        </p>
                     </div>
 
                     <div className="flex justify-end">
-                        <button className="h-16 w-1/2 text-md md:text-xl bg-white text-site-main rounded-xl hover:scale-[1.05] transform transition duration-y">
+                        <button className="text-sm md:text-md py-2 px-3 bg-white text-site-main hover:scale-[1.05] transform transition duration-y">
                             AVAIL NOW!
                         </button>
                     </div>
@@ -53,26 +53,26 @@ const SalesSection = () => {
                 </figure>
                 <div className="flex flex-col justify-between w-full gap-10 p-8 md:w-2/3">
                     <div className="flex flex-col">
-                        <span className="text-2xl md:text-5xl font-bold w-full">
+                        <p className="text-xl md:text-3xl font-bold w-full">
                             SHIRTS
-                        </span>
-                        <span className="text-md md:text-xl mt-4">
+                        </p>
+                        <p className="text-xs sm:text-sm md:text-md mt-4">
                             Secure your shirts now for an unforgettable
                             experience packed with incredible panels, exclusive
                             screenings, and epic cosplay. Whether you&apos;re a
                             long-time fan or new to the scene, Otakufest 2024
                             promises excitement.
-                        </span>
+                        </p>
                     </div>
 
                     <div className="flex justify-end">
-                        <button className="h-16 w-1/2 text-md md:text-xl bg-white text-site-main rounded-xl hover:scale-[1.05] transform transition duration-y">
+                        <button className="text-sm md:text-md py-2 px-3 bg-white text-site-main hover:scale-[1.05] transform transition duration-y">
                             AVAIL NOW!
                         </button>
                     </div>
                 </div>
             </div>
-            <span className="text-site-main text-justify text-md md:text-xl mt-4">
+            <span className="text-site-main text-center text-md md:text-xl mt-4">
                 For concerns regarding your purchases from Otakufest 2024,
                 consider{' '}
                 <Link href={Routes.HOME} className="underline">
