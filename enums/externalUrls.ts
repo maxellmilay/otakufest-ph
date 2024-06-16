25f6df6ad846d988707bcb5c6e672a1629a3daaa
Maxell Milay <milaymaxell@gmail.com>
2024-06-16 11:10:49 +0800

    [Adjustment][Maxell] rename link to url

---
+++ enums/externalUrls.ts
@@ -0,0 +1,7 @@
+enum ExternalUrls {
+    FB = 'https://facebook.com/otakufestph',
+    TWITTER = 'https://twitter.com/otakufestph',
+    INSTAGRAM = 'https://www.instagram.com/otakufestph',
+}
+
+export default ExternalUrls;
