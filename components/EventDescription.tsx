51c28c864aa6088f01a4fdfcef17bb5e2baf7178
Maxell Milay <milaymaxell@gmail.com>
2024-06-16 21:30:48 +0800

    [Adjustment][Maxell] adjusted event description responsiveness

--- components/EventDescription.tsx
+++ components/EventDescription.tsx
@@ -6,7 +6,7 @@ import { FaSquareXTwitter, FaSquareInstagram } from 'react-icons/fa6';
 
 const EventDescription = () => {
     return (
-        <div className="flex flex-col justify-center items-center text-center bg-site-main w-full h-[150vh] overflow-hidden relative">
+        <div className="flex flex-col justify-center items-center text-center bg-site-main w-full h-[75dvh] overflow-hidden relative">
             <div className="absolute bottom-0 flex flex-col h-1/2 w-full md:h-full md:w-1/2 md:right-0 z-[15] items-center text-center justify-center md:bg-gradient-to-l bg-gradient-to-t to-transparent from-site-main px-5 md:px-20">
                 <div className="flex flex-row">
                     <figure className="aspect-square w-10 md:w-16 relative">
@@ -17,11 +17,11 @@ const EventDescription = () => {
                             className="object-cover"
                         />
                     </figure>
-                    <span className="text-4xl md:text-6xl font-bold">
+                    <span className="text-4xl xl:text-6xl font-bold">
                         OTAKUFEST
                     </span>
                 </div>
-                <span className="text-md md:text-xl mt-4">
+                <span className="text-xs sm:text-md md:text-xl mt-4">
                     Otakufest is the premier and the most anticipated cosplay
                     and hobbies convention in the Visayas, held annually in Cebu
                     City, Philippines. <br />
@@ -29,14 +29,14 @@ const EventDescription = () => {
                     Come join our event happening this{' '}
                     <span className="font-bold"> AUGUST 17-18, 2024 </span> !
                 </span>
-                <div className="flex flex-row mt-16 gap-4">
-                    <FaFacebookSquare size={'5vh'} />
-                    <FaSquareXTwitter size={'5vh'} />
-                    <FaSquareInstagram size={'5vh'} />
+                <div className="flex flex-row mt-7 sm:mt-16 gap-4">
+                    <FaFacebookSquare size={'4vh'} />
+                    <FaSquareXTwitter size={'4vh'} />
+                    <FaSquareInstagram size={'4vh'} />
                 </div>
             </div>
             <div className="absolute top-0 flex-col w-full h-1/2 bg-gradient-to-b to-transparent from-site-main flex items-center justify-end z-10"></div>
-            <figure className="relative aspect-square w-[150vh] md:w-[200vh] md:ml-[-85vh]">
+            <figure className="relative aspect-square w-[75dvh] sm:w-[150dvh] md:w-[200dvh] md:ml-[-85vh]">
                 <Image
                     src={OF24_MAIN_VISUAL}
                     alt="of24visual"
