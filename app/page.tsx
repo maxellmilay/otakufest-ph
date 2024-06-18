163905c1fc677a095b31617776fbb53e2fc6a0a6
powtzulist <delasanarwin@gmail.com>
2024-06-19 00:14:49 +0800

    mergin my local with main

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import React from 'react';
-import { HomePage } from '@/components/HomePage';
-import EventDescription from '@/components/EventDescription';
-import GuestSection from '@/components/GuestSection';
-import SalesSection from '@/components/SalesSection';
-import EventSection from '@/components/EventSection';
-import SponsorSection from '@/components/SponsorSection';
-import BoothSection from '@/components/BoothSection';
+import { HomePage } from '@/components/home/HomePage';
+import EventDescription from '@/components/home/EventDescription';
+import GuestSection from '@/components/home/GuestSection';
+import SalesSection from '@/components/home/SalesSection';
+import EventSection from '@/components/home/EventSection';
+import SponsorSection from '@/components/home/SponsorSection';
+import BoothSection from '@/components/home/BoothSection';
 
 const Home = () => {
     return (
