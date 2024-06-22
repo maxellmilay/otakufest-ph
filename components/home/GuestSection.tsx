bd9384da5e1bde9b85e41c2ca57dfa931bc23fff
Maxell Milay <milaymaxell@gmail.com>
2024-06-22 10:45:28 +0800

    [Adjustment][Maxell] added guest card into section

--- components/home/GuestSection.tsx
+++ components/home/GuestSection.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import ComingSoon from '../generics/ComingSoon';
+import { GUESTS } from '@/enums/imageUrls';
+import GuestCard from './GuestCard';
 
 const GuestSection = () => {
     return (
-        <div className="flex flex-col items-center justify-around text-justify bg-white w-full p-10 md:px-10 md:py-16 lg:p-20">
-            <div className="flex flex-col text-site-main">
-                <p className="text-4xl lg:text-6xl font-bold w-full">GUESTS</p>
+        <div className="flex flex-col items-center justify-around text-justify bg-white w-full py-20">
+            <div className="flex flex-col text-site-main gap-10">
+                <p className="text-4xl lg:text-6xl font-bold w-full text-center">
+                    GUESTS
+                </p>
+                {GUESTS.map((guest, index) => {
+                    const isLeft = index % 2 == 0;
+
+                    return (
+                        <GuestCard
+                            name={guest.name}
+                            description={guest.description}
+                            imageUrl={guest.imageUrl}
+                            socials={guest.socials}
+                            key={index + guest.name}
+                            isLeft={isLeft}
+                        />
+                    );
+                })}
             </div>
-            <ComingSoon />
         </div>
     );
 };
