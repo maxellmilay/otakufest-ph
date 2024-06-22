e73b56a4b7d564d0b741ba82dbd667ee2e6fbb22
Loweso <dlouisefermin@gmail.com>
2024-06-22 14:51:00 +0800

    [UI][Louise] adjusted event image component for unrevealed events

--- components/home/EventImage.tsx
+++ components/home/EventImage.tsx
@@ -4,27 +4,36 @@ import Image from 'next/image';
 
 interface IProps {
     imageUrl: string;
+    blackImageURL: string;
     name: string;
     eventURL: string;
+    isRevealed: boolean;
 }
 
 const EventImage = (props: IProps) => {
-    const { imageUrl, name, eventURL } = props;
+    const { imageUrl, blackImageURL, name, eventURL, isRevealed } = props;
 
     const imageContent = (
         <figure className="relative aspect-square w-28 sm:w-52 md:w-52 lg:w-64 xl:w-96 hover:scale-105 duration-200">
-            <Image src={imageUrl} alt="guest" fill className="object-cover" />
+            <Image
+                src={isRevealed ? imageUrl : blackImageURL}
+                alt="guest"
+                fill
+                className="object-cover"
+            />
         </figure>
     );
 
     return (
         <div className="flex flex-col items-center text-center text-md md:text-xl gap-2">
-            {eventURL ? (
+            {isRevealed ? (
                 <Link href={eventURL}>{imageContent}</Link>
             ) : (
                 imageContent
             )}
-            <p className="text-xs md:text-sm lg:text-md xl:text-lg">{name}</p>
+            <p className="text-xs md:text-sm lg:text-md xl:text-lg">
+                {isRevealed ? name : '??? Competition'}
+            </p>
         </div>
     );
 };
