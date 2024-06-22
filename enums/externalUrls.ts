005db029a1a8cceae69a76d6696c454b1c600292
Maxell Milay <milaymaxell@gmail.com>
2024-06-22 11:11:29 +0800

    [Feature][Maxell] added pre order form in external links

--- enums/externalUrls.ts
+++ enums/externalUrls.ts
@@ -2,6 +2,7 @@ enum ExternalUrls {
     FB = 'https://facebook.com/otakufestph',
     TWITTER = 'https://twitter.com/otakufestph',
     INSTAGRAM = 'https://www.instagram.com/otakufestph',
+    PRE_ORDER_FORM = 'https://neartail.com/public/112519215755839682426/all/form/1FAIpQLScR8MZV6PUVhzaMnos3IEmig4c-oADinm_sEYiNhGGhhU4ltg',
 }
 
 export default ExternalUrls;
