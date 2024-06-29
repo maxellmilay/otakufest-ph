bfad64bff5bb9b2bbfbdac117e83a185c4c1b68a
Maxell Milay <milaymaxell@gmail.com>
2024-06-29 17:47:37 +0800

    [Feature][Maxell] added email api call in form

--- components/contact-us/ContactUsForms.tsx
+++ components/contact-us/ContactUsForms.tsx
@@ -1,6 +1,8 @@
 'use client';
+
 import React, { useState } from 'react';
 import { RiMailSendLine } from 'react-icons/ri';
+import Loader from '../generics/Loader';
 
 interface FormValues {
     name: string;
@@ -17,6 +19,8 @@ export const ContactUsForms = () => {
         question: '',
     });
 
+    const [isLoading, setIsLoading] = useState(false);
+
     const handleChange = (e: any) => {
         const { name, value } = e.target;
         setFormData({
@@ -25,70 +29,87 @@ export const ContactUsForms = () => {
         });
     };
 
+    const sendEmail = () => {
+        setIsLoading(true);
+        fetch('/api/send', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+        })
+            .then((response) => response.json())
+            .then((data) => alert('Concern Sent!'))
+            .catch((error) =>
+                alert('Concern was not sent, there was a problem on our end')
+            )
+            .finally(() => setIsLoading(false));
+    };
+
     const handleSubmit = (e: any) => {
         e.preventDefault();
         if (!formData.concern) {
             alert('Please select a concern.');
             return;
         }
-        console.log(formData);
+        sendEmail();
     };
 
     return (
-        <form className="flex flex-col gap-y-5 text-black font-lato w-[90%]">
-            <div>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Name..."
-                    value={formData.name}
-                    onChange={handleChange}
-                    className="py-2 px-4 w-full rounded-md"
-                />
-            </div>
-            <div>
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="E-mail Address..."
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="py-2 px-4 w-full  rounded-md"
-                />
-            </div>
-            <div>
-                <select
-                    name="concern"
-                    className="py-2 px-4  w-full rounded-md required bg-white"
-                    value={formData.concern}
-                    onChange={handleChange}
-                    required
-                >
-                    <option value="" disabled hidden>
-                        - Concern -
-                    </option>
-                    <option value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                </select>
-            </div>
-            <textarea
-                name="question"
-                className="h-32 rounded-md py-2 px-4"
-                placeholder="Your question here..."
-                value={formData.question}
-                onChange={handleChange}
-            ></textarea>
-
-            <button
-                type="submit"
-                className="w-full border-4 rounded-md px-4 py-2 mb-3 text-white flex items-center text-xl font-bold hover:bg-white hover:text-site-main hover:border-site-main"
-                onClick={handleSubmit}
-            >
-                <RiMailSendLine size={25} />
-                <div className="flex-grow text-center pr-3 ">
-                    <p>Submit</p>
+        <>
+            <form className="flex flex-col gap-y-5 text-black font-lato w-[90%]">
+                <div>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name..."
+                        value={formData.name}
+                        onChange={handleChange}
+                        className="py-2 px-4 w-full rounded-md"
+                    />
                 </div>
-            </button>
-        </form>
+                <div>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="E-mail Address..."
+                        value={formData.email}
+                        onChange={handleChange}
+                        className="py-2 px-4 w-full  rounded-md"
+                    />
+                </div>
+                <div>
+                    <select
+                        name="concern"
+                        className="py-2 px-4  w-full rounded-md required bg-white"
+                        value={formData.concern}
+                        onChange={handleChange}
+                        required
+                    >
+                        <option value="" disabled hidden>
+                            - Concern -
+                        </option>
+                        <option value="option1">Option 1</option>
+                        <option value="option2">Option 2</option>
+                    </select>
+                </div>
+                <textarea
+                    name="question"
+                    className="h-32 rounded-md py-2 px-4"
+                    placeholder="Your question here..."
+                    value={formData.question}
+                    onChange={handleChange}
+                ></textarea>
+
+                <button
+                    type="submit"
+                    className="w-full border-4 rounded-md px-4 py-2 mb-3 text-white flex items-center text-xl font-bold hover:bg-white hover:text-site-main hover:border-site-main"
+                    onClick={handleSubmit}
+                >
+                    <RiMailSendLine size={25} />
+                    <div className="flex-grow text-center pr-3 ">
+                        <p>Submit</p>
+                    </div>
+                </button>
+            </form>
+            <Loader isLoading={isLoading} />
+        </>
     );
 };
