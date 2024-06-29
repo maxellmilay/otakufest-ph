a264f74de7bd958b0118fd24693c92254dc89fd0
Maxell Milay <milaymaxell@gmail.com>
2024-06-29 17:44:54 +0800

    [Feature][Maxell] added nodemailer send email logic

---
+++ utils/mail.utils.ts
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.MAIL_EMAIL,
+        pass: process.env.MAIL_PASSWORD,
+    },
+} as SMTPTransport.Options);
+
+type SendEmailDto = {
+    sender: Mail.Address;
+    receipients: Mail.Address[];
+    subject: string;
+    message: string;
+    html: string;
+};
+
+export const sendEmail = async (dto: SendEmailDto) => {
+    const { sender, receipients, subject, message, html } = dto;
+
+    return await transport.sendMail({
+        from: sender,
+        to: receipients,
+        html: html,
+        text: message,
+        subject: subject,
+    });
+};
